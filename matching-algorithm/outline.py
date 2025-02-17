07c1b65825092cb370ee301f252d9d17796a79cd
James Truitt <jtruitt1@swarthmore.edu>
2025-02-17 14:44:36 -0500

    Edits to algorithm

---
+++ matching-algorithm/outline.py
@@ -0,0 +1,27 @@
+import csv
+
+def Persona():
+    pass
+
+def Invidivual():
+    pass
+
+# Load data from CSVs into Events
+
+events = []
+
+# Create persona list from events
+personas = []
+
+for event in events:
+    # Do things to event to generate personas
+    pass
+
+    # Add newly made personas to persona container
+
+# Do shallow blocking of personas
+personaGroups = []
+
+# Start creating individuals from persona groups
+individuals = []
+
